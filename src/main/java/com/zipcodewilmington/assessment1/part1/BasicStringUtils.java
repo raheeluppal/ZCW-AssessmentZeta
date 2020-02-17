1c85780914f430d4a3ef0cd67119dfb31b31e061
Raheel Uppal <ruppal@zipcodes-MacBook-Pro.local>
2020-02-17 09:46:39 -0500

    2

--- src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/BasicStringUtils.java
@@ -9,7 +9,8 @@ public class BasicStringUtils {
      * @return string with identical content, and the first character capitalized
      */
     public static String camelCase(String str) {
-        return null;
+
+        return str.substring(0, 1).toUpperCase() + str.substring(1);
     }
 
     /**
@@ -17,7 +18,8 @@ public class BasicStringUtils {
      * @return string with identical contents, in the reverse order
      */
     public static String reverse(String str) {
-        return null;
+
+        return new StringBuilder(str).reverse().toString();
     }
 
     /**
@@ -25,7 +27,9 @@ public class BasicStringUtils {
      * @return string with identical contents, in reverse order, with first character capitalized
      */
     public static String reverseThenCamelCase(String str) {
-        return null;
+        String result = new StringBuilder(str).reverse().toString();
+
+        return result.substring(0, 1).toUpperCase() + result.substring(1);
     }
 
 
@@ -34,7 +38,8 @@ public class BasicStringUtils {
      * @return string with identical contents excluding first and last character
      */
     public static String removeFirstAndLastCharacter(String str) {
-        return null;
+
+            return str.substring(1, str.length() - 1);
     }
 
     /**
@@ -42,6 +47,20 @@ public class BasicStringUtils {
      * @return string with identical characters, each with opposite casing
      */
     public static String invertCasing(String str) {
-        return null;
+
+        char[] chars = str.toCharArray();
+        for (int i = 0; i < chars.length; i++)
+        {
+            char c = chars[i];
+            if (Character.isUpperCase(c))
+            {
+                chars[i] = Character.toLowerCase(c);
+            }
+            else if (Character.isLowerCase(c))
+            {
+                chars[i] = Character.toUpperCase(c);
+            }
+        }
+        return new String(chars);
     }
 }
--- src/main/java/com/zipcodewilmington/assessment1/part1/IntegerArrayUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/IntegerArrayUtils.java
@@ -9,7 +9,13 @@ public class IntegerArrayUtils {
      * @return the sum of `intArray`
      */
     public static Integer getSum(Integer[] intArray) {
-        return null;
+        Integer result = 0;
+
+        for (int i = 0; i <=intArray.length-1 ; i++) {
+            result += intArray[i];
+
+        }
+        return result;
     }
 
     /**
@@ -17,7 +23,14 @@ public class IntegerArrayUtils {
      * @return the product of `intArray`
      */
     public static Integer getProduct(Integer[] intArray) {
-        return null;
+
+        Integer result = 1;
+
+        for (int i = 0; i <=intArray.length-1 ; i++) {
+            result *= intArray[i];
+
+        }
+        return result;
     }
 
     /**
@@ -25,6 +38,12 @@ public class IntegerArrayUtils {
      * @return the sum of `intArray` divided by number of elements in `intArray`
      */
     public static Double getAverage(Integer[] intArray) {
-        return null;
+        Double result = 0.0;
+
+        for (int i = 0; i <= intArray.length - 1; i++) {
+            result += intArray[i];
+        }
+
+        return (result / intArray.length);
     }
 }
