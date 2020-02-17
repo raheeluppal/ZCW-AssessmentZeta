147f62c4aa0c86d0287e0e95e0d2515274b9232c
Raheel Uppal <ruppal@zipcodes-MacBook-Pro.local>
2020-02-17 10:06:35 -0500

    part 1

--- src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/IntegerUtils.java
@@ -11,7 +11,15 @@ public class IntegerUtils {
      * @return the sum of all integers between 0 and not including `n`
      */
     public static Integer getSumOfN(Integer n) {
-        return null;
+
+        Integer result = 0;
+
+        for (int i = 0; i <= n ; i++) {
+
+            result += i;
+
+        }
+        return result;
     }
 
     /**
@@ -19,7 +27,15 @@ public class IntegerUtils {
      * @return the product of all integers between 0 and not including `n`
      */
     public static Integer getProductOfN(Integer n) {
-        return null;
+
+        Integer result = 1;
+
+        for (int i = 1; i <= n ; i++) {
+
+            result *= i;
+
+        }
+        return result;
     }
 
     /**
@@ -27,6 +43,13 @@ public class IntegerUtils {
      * @return integer with identical digits in the reverse order
      */
     public static Integer reverseDigits(Integer val) {
-        return null;
+
+        int reversed = 0;
+        while(val != 0) {
+            int digit = val % 10;
+            reversed = reversed * 10 + digit;
+            val /= 10;
+        }
+        return reversed;
     }
 }
--- src/main/java/com/zipcodewilmington/assessment1/part1/RockPaperSissorsEvaluator.java
+++ src/main/java/com/zipcodewilmington/assessment1/part1/RockPaperSissorsEvaluator.java
@@ -13,7 +13,14 @@ public class RockPaperSissorsEvaluator {
      * @return the respective winning move
      */
     public String getWinningMove(String handSign) {
-        return null;
+        if (handSign.equals("rock")) {
+            return "paper";
+        } else if (handSign.equals("paper")) {
+            return "scissor";
+        } else {
+
+        }
+        return "rock";
     }
 
     /**
@@ -21,7 +28,15 @@ public class RockPaperSissorsEvaluator {
      * @return the respective losing move
      */
     public String getLosingMove(String handSign) {
-        return null;
+
+        if (handSign.equals("rock")) {
+            return "scissor";
+        } else if (handSign.equals("paper")) {
+            return "rock";
+        } else {
+
+        }
+        return "paper";
     }
 
     /**
@@ -30,6 +45,19 @@ public class RockPaperSissorsEvaluator {
      * @return a string representative of the winning hand sign between the two players
      */
     public String getWinner(String handSignOfPlayer1, String handSignOfPlayer2) {
-        return null;
+        if (handSignOfPlayer1.equals("rock") && handSignOfPlayer2.equals("paper")) {
+            return "paper";
+        } else if (handSignOfPlayer1.equals("rock") && handSignOfPlayer2.equals("scissor")) {
+            return "rock";
+        } else if (handSignOfPlayer1.equals("scissor") && handSignOfPlayer2.equals("rock")) {
+            return "rock";
+        } else if (handSignOfPlayer1.equals("scissor") && handSignOfPlayer2.equals("paper")) {
+            return "scissor";
+        } else if (handSignOfPlayer1.equals("paper") && handSignOfPlayer2.equals("rock")) {
+            return "paper";
+        }
+        else {
+            return "scissor";
+        }
     }
 }
